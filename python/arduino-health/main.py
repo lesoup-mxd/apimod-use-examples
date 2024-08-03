152dd8252c34eb8c8b08e77dce117ac95774cfef
lesoup-mxd <le.soup@hotmail.com>
2024-08-03 16:15:57 +0300

    Fixed delay, newline issues

--- python/arduino-health/main.py
+++ python/arduino-health/main.py
@@ -19,7 +19,6 @@ import time
 def write_ser(x, ser):
     ser.write(bytes(x, 'utf-8'))  # Send data to Arduino
     time.sleep(0.05)  # Short delay to allow Arduino to process
-    return None
 
 def get_health_level():
     url = "http://localhost:8080/hello" #Disclaimer: the endpoint may change as the project evolves
@@ -27,25 +26,37 @@ def get_health_level():
     if response.status_code == 200: #"OK"
         data = response.json()
         health_level = data.get("health") #Health key
+        #For debugging purposes
+        #health_level = 20 #Debug !
         if health_level is not None:
             # Directly use the health level as it's within 0-20 (Should be a float in the json response)
-            #print(f"Health level: {health_level}") #Debug
-            return float(health_level)
+                print(f"Health level: {health_level}") #Debug
+                return float(health_level)
     else:
         print(f"Failed to fetch health level, status code: {response.status_code}")
         return None
 
 if __name__ == "__main__":
     try:
+        health_level = 5
+        new_health_level = 0
         ser = serial.Serial('/dev/ttyUSB0', 9600)
+        time.sleep(1.5)  # Wait for Arduino to reset
+        write_ser('0\n', ser)  # Send 0 to Arduino to reset the health level
         while True: #Main loop
-            health_level = get_health_level()
+              # Delay
+            new_health_level = get_health_level()
+            if health_level == new_health_level:
+                time.sleep(.3)
+                continue
+            time.sleep(.5)
+            health_level = new_health_level
+
+
             if health_level is not None:
                 write_ser(
-                    str(
-                        int(health_level * 12.75)),
+                    (str(int(health_level * 12.75))+'\n'),
                     ser=ser)  # Map health level to 0-255 and send to Arduino
-            time.sleep(.2)  # Delay
     except KeyboardInterrupt:
         print("Exiting...")
         exit()
